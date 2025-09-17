c8b8eeea49c53fb2bd5c716531d83483ae0f10ad
Aashish Shrestha <aashishshreestha@gmail.com>
2025-09-17 14:49:58 +0545

    initial project setup with basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,13 @@ import Skills from "./components/skills/Skills";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [pathname]);
-
-  return null;
-};
-
 const App = () => {
   return (
     <div>
       <Navbar />
-      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
